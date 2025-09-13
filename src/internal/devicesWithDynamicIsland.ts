a86a3eecf944346d499385531705f9f944d35041
zaferatli <zaferr@protonmail.com>
2025-09-13 08:40:09 +0300

    feat(ios): add missing iPhone 17 & 16e device names

--- src/internal/devicesWithDynamicIsland.ts
+++ src/internal/devicesWithDynamicIsland.ts
@@ -3,6 +3,26 @@ import { NotchDevice } from './privateTypes';
 const devicesWithDynamicIsland: NotchDevice[] = [
   {
     brand: 'Apple',
+    model: 'iPhone 17',
+  },
+  {
+    brand: 'Apple',
+    model: 'iPhone 17 Air',
+  },
+  {
+    brand: 'Apple',
+    model: 'iPhone 17 Pro',
+  },
+  {
+    brand: 'Apple',
+    model: 'iPhone 17 Pro Max',
+  },
+  {
+    brand: 'Apple',
+    model: 'iPhone 16e',
+  },
+  {
+    brand: 'Apple',
     model: 'iPhone 16',
   },
   {
